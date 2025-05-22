78088098f828f14c6c0da011e061811a65eedbb8
Caique <caiquedebritoo@gmail.com>
2025-05-21 21:22:15 -0300

    Add project depencies and configuration

--- control_panel.c
+++ control_panel.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
-
+#include "FreeRTOS.h"
+#include "task.h"
+#include "hardware/pwm.h"
+#include "hardware/clocks.h"
+#include "hardware/i2c.h"
+#include "lib/ssd1306.h"
 
 
 int main()
